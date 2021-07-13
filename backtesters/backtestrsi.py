ca7be493af6cb4d3047530650d91a2c1706e829a
PhumLWC <jeremyvuong.dshs@gmail.com>
2021-07-13 17:04:42 +0800

    successfully created working backtest function which accepts arguments in json form when post request is sent to endpoint

--- backtesters/backtestrsi.py
+++ backtesters/backtestrsi.py
@@ -13,6 +13,7 @@ from enums import *
 from datetime import datetime, timezone
 import schedule
 import btalib
+import json
 
 #init environ variables
 api_key = os.environ.get('API_KEY')
@@ -55,6 +56,46 @@ def backtest_rsi():
     df_search_size = 30
 
     result = backtest_repository.run_backtest(TICKER, starting_fiat_balance, trading_fee, algolist, algoargs, KlineInterval._1DAY.value, fiat_percent, coin_percent, starting_date, ending_date, df_search_size)
+    return result
+
+# function which accepts a dictionary for backtest arguments
+def backtest_rsi_external(args):
+    print(args)
+    TICKER = args["ticker"]
+    starting_fiat_balance = args["starting_fiat_balance"]
+    starting_coin_balance = args["starting_coin_balance"]
+
+    trading_fee = args["trading_fee"]
+
+    algostringlist = args["algolist"]
+    #sort through string list and get function references
+    algolist = []
+    for algo in algostringlist:
+        algolist.append(string_to_reference[algo])
+    algoargs = args["algoargs"]
+
+    KlineInterval = args["klineinterval"]
+
+    fiat_percent = args["fiat_percent"]
+    coin_percent = args["coin_percent"]
+
+    starting_date = args["starting_date"]
+    ending_date = args["ending_date"]
+
+    df_search_size = args["df_search_size"]
+
+    # init
+    client = Client(api_key, api_secret)
+
+    b_controller = BinanceController(client)
+
+    botcontext = {
+        'controller': b_controller,
+    }
+
+    backtest_repository = BacktestRepository(botcontext)
+
+    result = backtest_repository.run_backtest(TICKER, starting_fiat_balance, trading_fee, algolist, algoargs, KlineInterval, fiat_percent, coin_percent, starting_date, ending_date, df_search_size)
     return result
 
 if __name__ == '__main__':
--- myapi/views.py
+++ myapi/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
 from rest_framework import viewsets
 
 from .serializers import BacktestResultsSerializer
@@ -13,6 +18,8 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
 
+from backtesters.backtestrsi import backtest_rsi, backtest_rsi_external
+
 # Create your views here
 
 class BacktestResultsSetClass(viewsets.ModelViewSet):
@@ -21,7 +28,12 @@ class BacktestResultsSetClass(viewsets.ModelViewSet):
 
 class BacktestResultsSet(APIView):
     def post(self, request, format=None):
-        
-        test = BacktestResults(fiat_balance=123, coin_balance=123, win_rate=41)
-        serializer = BacktestResultsSerializer(test)
+
+        #result = backtest_rsi()
+        result = backtest_rsi_external(request.data) #pass in json arguments
+        resultmodel = BacktestResults(fiat_balance=result[0], coin_balance=result[1], win_rate=result[2])
+        serializer = BacktestResultsSerializer(resultmodel)
+
+        #if serializer.is_valid():
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+        #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
